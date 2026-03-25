ca6c83644a5ae50f021e33b004f78658c522786c
madaiyin777 <daiyin.ma@gmail.com>
2026-03-26 00:53:16 +0800

    add server/src/auth/jwt.strategy.ts

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+// ========================
+// JWT 策略
+// ========================
+
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    config: ConfigService,
+    private authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET', 'default-secret'),
+    });
+  }
+
+  async validate(payload: any) {
+    return this.authService.validateUser(payload);
+  }
+}
